6a34f262a71ac2396e3b9c44455bd6a918bc99ab
Jack Riminton <jriminton@gmail.com>
2016-05-22 10:43:28 +0100

    route change

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,5 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-  root 'application#hello'
+  # root 'application#hello'
 end
